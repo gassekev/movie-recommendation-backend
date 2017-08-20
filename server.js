a0fdedc0aa00de77d893515ca1316cb5c6afc395
Simon Mueller <13031695@student.uts.edu.au>
2017-08-20 18:43:15 +1000

    add express server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('penis');
+import express from 'express';
+import httpStatus from 'http-status';
+import cors from 'cors';
+import { json as jsonBodyParser } from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(jsonBodyParser());
+
+app.get('/', (req, res) => {
+  res.status(httpStatus.OK).json({});
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
