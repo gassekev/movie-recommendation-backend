716e0a6c13ad3a3b36fc212c7382c760f969b9e8
Tobias Huonder <tobias.c.huonder@student.uts.edu.au>
2017-08-20 22:42:54 +1000

    change port

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.post('/', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
 });
