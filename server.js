99e4ad3cacba37b6d72513447ef0e9451534bea0
Simon Mueller <13031695@student.uts.edu.au>
2017-08-24 15:29:50 +1000

    factor out db connection to file

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-import mongodb from 'mongodb';
 import httpStatus from 'http-status';
 import { json as jsonBodyParser } from 'body-parser';
+import { dbMiddleware } from './src/db';
 
 const app = express();
-const MongoClient = mongodb.MongoClient;
-let db = null;
 
 app.use(cors());
 app.use(jsonBodyParser());
+app.use(dbMiddleware);
 
 app.get('/recommendations', (req, res) => {
   const recommendedMovies = ['Die Hard 4.0', 'Fast 10: your seatbelts',
@@ -18,7 +17,7 @@ app.get('/recommendations', (req, res) => {
 });
 
 app.post('/watchedmovie', (req, res) => {
-  const collection = db.collection('watchedmovies');
+  const collection = req.db.collection('watchedmovies');
   collection.insert(req.body)
     // TODO: Remove test output
     .then(() => collection.find().toArray())
@@ -29,10 +28,6 @@ app.post('/watchedmovie', (req, res) => {
     .catch(() => res.sendStatus(httpStatus.BAD_REQUEST));
 });
 
-// Connect to the mongodb and start the server
-MongoClient.connect('mongodb://mongo:27017/movierec')
-  .then((database) => { db = database; })
-  .then(() => app.listen(3001, () => {
-    console.log('Listening on port 3001');
-  }))
-  .catch(err => console.log(err.message));
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
