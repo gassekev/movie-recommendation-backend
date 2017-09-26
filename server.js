eb538cc1276e371778f4c5c761dc36a369ae08ef
Simon Mueller <13031695@student.uts.edu.au>
2017-09-27 00:23:10 +1000

    remove testuser

--- server.js
+++ server.js
@@ -2,25 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import { json as jsonBodyParser } from 'body-parser';
 import { connectToDB } from './src/data/db';
-import User from './src/data/model/user';
 import controller from './src/controller/';
 
 const app = express();
-
-// TODO: remove testUser
-// START TESTING
-const testUser = {
-  username: 'aip',
-  email: 'aip@student.uts.edu.au',
-  seenMovies: [
-    'Fast 10: your seatbelts',
-  ],
-};
-
-// create test user
-User.create(testUser)
-  .catch(err => console.log(err.message));
-// END TESTING
 
 app.use(cors());
 app.use(jsonBodyParser());
