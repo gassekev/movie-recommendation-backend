dea9552f8fc7c8cca0c011305e768c1121a49991
Simon Mueller <13031695@student.uts.edu.au>
2017-09-27 11:48:20 +1000

    rename of router to reflect model

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import movieRouter from './router/movie';
+import userRouter from './router/user';
 import authRouter from './router/auth';
 import authMiddleware from './middleware/auth';
 
 const router = new Router();
 
-router.use('/api/movie', authMiddleware, movieRouter);
+router.use('/api/user', authMiddleware, userRouter);
 router.use('/api/auth', authRouter);
 
 export default router;
