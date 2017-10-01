802b43fae3c2835e8e3128825ae2fce6257b9476
Simon Mueller <13031695@student.uts.edu.au>
2017-10-01 15:16:57 +1100

    add error

--- src/controller/middleware/error.js
+++ src/controller/middleware/error.js
@@ -7,6 +7,12 @@ export default function (err, req, res, next) {
     case 'UnauthorizedError':
       res.status(httpStatus.UNAUTHORIZED).json({ error: 'invalid token...' });
       break;
+    case 'ValidationError':
+      res.status(httpStatus.BAD_REQUEST).json({ error: err.message });
+      break;
+    case 'AuthError':
+      res.status(httpStatus.BAD_REQUEST).json({ error: err.message });
+      break;
     default:
       res.sendStatus(httpStatus.BAD_REQUEST);
   }
