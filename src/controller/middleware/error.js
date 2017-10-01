a1526b18f172bc0c61d8efcb36f9cb4741354090
Simon Mueller <13031695@student.uts.edu.au>
2017-10-01 14:33:57 +1100

    add more generic unauthorized message

--- src/controller/middleware/error.js
+++ src/controller/middleware/error.js
@@ -1,9 +1,11 @@
 import httpStatus from 'http-status';
 
 export default function (err, req, res, next) {
+  console.log(err);
+
   switch (err.name) {
     case 'UnauthorizedError':
-      res.status(httpStatus.UNAUTHORIZED).json({ error: err.message });
+      res.status(httpStatus.UNAUTHORIZED).json({ error: 'invalid token...' });
       break;
     default:
       res.sendStatus(httpStatus.BAD_REQUEST);
