ac918fd4f6e0252a3406056ab159bba47d2d6afa
Simon Mueller <13031695@student.uts.edu.au>
2017-10-14 19:10:55 +1100

    minor api change

--- src/controller/router/movieRouter.js
+++ src/controller/router/movieRouter.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = new Router();
 
 router.get('/', (req, res) => {
-  res.json({ movies: ['Test Movie 1', 'Test Movie 2'] });
+  res.json(['Test Movie 1', 'Test Movie 2']);
 });
 
 export default router;
